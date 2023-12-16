1c6d3ff0bc670c86438dfde4aefc97897161fd61
Ivan Zabunyan <ivanes@list.ru>
2023-12-16 17:42:06 +0300

    start set

--- apps/front/src/main.tsx
+++ apps/front/src/main.tsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react';
+import {StrictMode} from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
+import {App} from "./app/app";
 
-import App from './app/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <App/>
     </BrowserRouter>
   </StrictMode>
 );
